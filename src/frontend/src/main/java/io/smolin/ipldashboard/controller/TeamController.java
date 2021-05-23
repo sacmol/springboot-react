044c5ec2e9c65ce67d585899950647a058c4b936
smolin <J10sacham@gmail.com>
2021-05-23 20:59:48 +0200

    files added

---
+++ src/frontend/src/main/java/io/smolin/ipldashboard/controller/TeamController.java
@@ -0,0 +1,32 @@
+package io.smolin.ipldashboard.controller;
+
+import io.smolin.ipldashboard.model.Team;
+import io.smolin.ipldashboard.repository.MatchRepository;
+import io.smolin.ipldashboard.repository.TeamRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class TeamController {
+
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName) {
+
+        Team team = this.teamRepository.findByTeamName(teamName);
+        team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4));
+        return team;
+    }
+}
